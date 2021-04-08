31ab669bd3dfe7e08d6fd24b6e27870f8fa7fc57
Ergi Ymeri <ymeriergi@gmail.com>
2021-04-08 14:52:09 +0200

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,17 @@ Route::apiResource('/question/{question}/reply', 'ReplyController');
 // Like Routes
 Route::post('/like/{reply}', 'LikeController@likeIt');
 Route::delete('/like/{reply}', 'LikeController@unLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
